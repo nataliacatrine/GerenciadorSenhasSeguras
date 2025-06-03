59f7bd3f101b57241b13cb3685400b2ef492cc3d
Natália Catrine <nataliacatrine@outlook.com.br>
2025-06-03 19:03:04 -0300

    Modo offline na verificação de senhas vazadas via API

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -207,7 +207,6 @@ public class Main {
                     break;
             }
         }
-
         scanner.close();
     }
 }
--- src/main/java/util/VerificadorVazamentoSenha.java
+++ src/main/java/util/VerificadorVazamentoSenha.java
@@ -33,7 +33,6 @@ public class VerificadorVazamentoSenha {
                 }
             }
         }
-
         return false; // Senha não vazada
     }
 
